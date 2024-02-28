a8bffbb342c4bb79403a231fc40cf13ffd5fc1a6
120210474 ALINE DE BRITO DAS NEVES <aline.neves@lcc1-23.lcc.ufcg.edu.br>
2024-02-28 15:57:33 -0300

    criação de pasta

---
+++ processador/src/testes/BoletoTest.java
@@ -0,0 +1,15 @@
+package testes;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoletoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
